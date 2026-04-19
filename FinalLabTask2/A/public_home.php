39fe3d39cd53042b1b431aea9ce9b45d4b0d6cf7
Shihab <shihab@example.com>
2026-04-19 14:36:29 +0600

    Added Public Home and Logo

---
+++ FinalLabTask2/A/public_home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Public Home</title>
+</head>
+<body>
+    <table border="1" width="800px" cellspacing="0" align="center">
+        <tr height="100px">
+            <td>
+                <img src="logo.png" alt="xCompany" width="40px">
+                
+                <div style="float: right;">
+                    <a href="public_home.php">Home</a> | 
+                    <a href="login.php">Login</a> | 
+                    <a href="registration.php">Registration</a>
+                </div>
+            </td>
+        </tr>
+
+        <tr height="250px">
+            <td>
+                <br>
+                <h3>&nbsp;&nbsp;Welcome to xCompany</h3>
+                <br>
+            </td>
+        </tr>
+
+        <tr height="50px">
+            <td align="center">
+                Copyright © 2017
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
